85df72e6ad4bb83767f4373ae4f887b441a43b66
vishnu prasad u <38067602+vishnuprasadu@users.noreply.github.com>
2021-05-11 13:28:44 +0530

    Update PrintSum.java

--- PrintSum.java
+++ PrintSum.java
@@ -2,6 +2,8 @@ public class PrintSum {
   public static void main(String[] args) {
     System.out.println("Hello This Is A Demo Java Jenkin");
     int sum = 5+5;
+    int sub = 10-5;
     System.out.println("The Sum Of 5+5 "+sum);
+    System.out.println("The Diff Of 10-5 "+sub);
   }
 }
